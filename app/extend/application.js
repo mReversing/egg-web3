cab2586ddf845c7f86c85f0e28a2aa245e702071
CallMeZ <callme-z@qq.com>
2018-07-01 02:02:45 +0800

    no message

--- app/extend/application.js
+++ app/extend/application.js
@@ -1,19 +1,13 @@
 'use strict';
 
-const Web3 = require('web3');
-const ZeroClientProvider = require('web3-provider-engine/zero.js');
+const loader = require('../../lib/loader');
 
 const WEB3 = Symbol('Application#web33');
 
-// 为了兼容数据(etherscan为小写,但是web3是区分大小写)一致, 全部hack为小写
-// @see https://ethereum.stackexchange.com/questions/2045/is-ethereum-wallet-address-case-sensitiveif (Web3.utils.toChecksumAddress) Web3.utils.toChecksumAddress = address => address.toLowerCase();
-
 module.exports = {
-
   get web3() {
     if (!this[WEB3]) {
-      const providerEngine = ZeroClientProvider(this.config.web3);
-      this[WEB3] = new Web3(providerEngine);
+      this[WEB3] = loader(this);
     }
     return this[WEB3];
   },
--- config/config.default.js
+++ config/config.default.js
@@ -1,4 +1,5 @@
 'use strict';
 
 exports.web3 = {
+  engine: 'web3', // web3, zero
 };
